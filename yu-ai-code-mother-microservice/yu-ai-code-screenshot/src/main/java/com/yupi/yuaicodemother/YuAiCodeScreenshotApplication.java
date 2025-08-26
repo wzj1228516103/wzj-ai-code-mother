16782f8642df912bf15c0bc08e4de3059ba89052
1-鱼皮 <592789970@qq.com>
2025-08-26 22:15:17 +0800

    教程第 14 期 - 微服务改造 - 截图服务（注意，这次提交为中间态，影响了单体项目代码，不要单独使用当前提交）

---
+++ yu-ai-code-mother-microservice/yu-ai-code-screenshot/src/main/java/com/yupi/yuaicodemother/YuAiCodeScreenshotApplication.java
@@ -0,0 +1,11 @@
+package com.yupi.yuaicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuAiCodeScreenshotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YuAiCodeScreenshotApplication.class, args);
+    }
+}
